5092ed4ca12eb503dc6fdfd962e8e193db7ce362
Sumaiyah Chinoy <sumaiyahchinoy@gmail.com>
2023-04-02 16:28:10 +0500

    Created React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+//import { PowerBIEmbed } from "powerbi-client-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        title="Report Section"
+        width="1024"
+        height="1060"
+        src="https://app.powerbi.com/view?r=eyJrIjoiODE2YWZlNmUtNDJkNi00ZDY2LWIxMmItY2I1ZTc2MGQxNTM0IiwidCI6ImZlZTNiOTE2LTAxYzEtNDk4Ny1hNjQ2LWUxOTM0MzJiOWVhYSIsImMiOjl9"
+        frameborder="0"
+        allowFullScreen="true"
+      ></iframe>
     </div>
   );
 }
